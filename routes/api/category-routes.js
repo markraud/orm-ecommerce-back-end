1ab61d493180af69f1f2ecec6647b9c43904d0f1
Mark Raudenbush <markraud@hotmail.com>
2021-05-12 19:56:00 -0600

    completed all the category-routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -36,16 +36,47 @@ router.get('/:id', async (req, res) => {
 });
 
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try {
+    const catagoryData = await Category.create(req.body);
+    res.status(200).json(catagoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    },
+  })
+    .then((catagoryData) => res.status(200).json(catagoryData))
+    .catch((err) => res.status(500).json(err))
+
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const catagoryData = await Category.destroy({
+      where: {
+        id: req.params.id
+      }
+    });
+
+    if (!catagoryData) {
+      res.status(404).json({ message: 'No catagory found with this id!' });
+      return;
+    }
+
+    res.status(200).json(catagoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
