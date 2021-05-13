e83d1d442fbecc4252771a50c2e6458e63194445
Mark Raudenbush <markraud@hotmail.com>
2021-05-12 18:28:15 -0600

    first attempt at built route for catagory by ID

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -19,7 +19,22 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const catagoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product }]
+    });
+
+    if (!catagoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(catagoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
+
 
 router.post('/', (req, res) => {
   // create a new category
